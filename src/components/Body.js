87cc2202cb4246a6ef3582b65aacac1223f57a28
muhsin sutanto <muhsin@femaledaily.com>
2019-12-11 18:22:26 +0700

    update set-up

---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  state = {};
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>pay</li>
+          <li>promo</li>
+          <li>top up</li>
+          <li>more</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Body;
