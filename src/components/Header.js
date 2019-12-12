b1baffb4877863a7227c5e83195f41e58aa03651
muhsin sutanto <muhsin@femaledaily.com>
2019-12-12 13:35:12 +0700

    apply muliple contexts-hooks on functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,20 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { LayoutContext } from "../context/LayoutContext";
 import { AuthContext } from "../context/AuthContext";
 
-class Header extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {authContext => (
-          <LayoutContext.Consumer>
-            {LayoutContext => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { nightMode, day, night } = LayoutContext;
-              const mood = nightMode ? night : day;
-              return (
-                <div>
-                  <div style={{ background: mood.bg }}>
-                    <h2>gopay</h2>
-                  </div>
-                  <button onClick={toggleAuth}>
-                    {isAuthenticated ? <p>logged in </p> : <p>Logged out</p>}
-                  </button>
-                </div>
-              );
-            }}
-          </LayoutContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
+const Header  = () => {
+  const {nightMode, day, night} = useContext(LayoutContext)
+  const {isAuthenticated, toggleAuth} = useContext(AuthContext)
+
+  const mood = nightMode ? night : day;
+  return ( <div>
+    <div style={{ background: mood.bg }}>
+      <h2>gopay</h2>
+    </div>
+    <button onClick={toggleAuth}>
+      {isAuthenticated ? <p>logged in </p> : <p>Logged out</p>}
+    </button>
+  </div> );
 }
 
 export default Header;
--- src/components/ToggleLayout.js
+++ src/components/ToggleLayout.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import { LayoutContext } from '../context/LayoutContext';
+import React, { useContext } from "react";
+import { LayoutContext } from "../context/LayoutContext";
 
-class ToggleLayout extends Component {
-  render() { 
-    return ( <LayoutContext.Consumer>{context => (
-      <button onClick={context.toggleLayout}>Change mood</button> 
-    )}
-    </LayoutContext.Consumer> );
-  }
-}
- 
-export default ToggleLayout ;
+const ToggleLayout = () => {
+  const { toggleLayout } = useContext(LayoutContext);
+  return <button onClick={toggleLayout}>Change mood</button>;
+};
+
+export default ToggleLayout;
