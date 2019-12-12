418432be38f611fac83187cb4fbf3d21ce595057
muhsin sutanto <muhsin@femaledaily.com>
2019-12-12 13:23:36 +0700

    apply useContext from hooks on functional component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,19 @@
-import React, { Component } from "react";
-import {LayoutContext} from '../context/LayoutContext'
+import React, { useContext } from "react";
+import { LayoutContext } from "../context/LayoutContext";
 
-class Body extends Component {
-  state = {};
-  render() {
-    return (
-      <LayoutContext.Consumer>{(context) => {
-        const {nightMode, day, night} = context
-        const mood = nightMode ? night : day
-        return(
-          <nav>
-            <ul>
-              <li style={{background: mood.bg, color: mood.color}}>pay</li>
-              <li style={{background: mood.bg, color: mood.color}}>promo</li>
-              <li style={{background: mood.bg, color: mood.color}}>top up</li>
-              <li style={{background: mood.bg, color: mood.color}}>more</li>
-            </ul>
-          </nav>
-        )
-      }}
-      </LayoutContext.Consumer>
-    );
-  }
-}
+const Body = () => {
+  const { nightMode, day, night } = useContext(LayoutContext);
+  const mood = nightMode ? night : day;
+  return (
+    <nav>
+      <ul>
+        <li style={{ background: mood.bg, color: mood.color }}>pay</li>
+        <li style={{ background: mood.bg, color: mood.color }}>promo</li>
+        <li style={{ background: mood.bg, color: mood.color }}>top up</li>
+        <li style={{ background: mood.bg, color: mood.color }}>more</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import {LayoutContext} from '../context/LayoutContext'
-import {AuthContext} from '../context/AuthContext'
+import { LayoutContext } from "../context/LayoutContext";
+import { AuthContext } from "../context/AuthContext";
 
 class Header extends Component {
   render() {
     return (
       <AuthContext.Consumer>
-        {(authContext) => (
-           <LayoutContext.Consumer>
-           {(LayoutContext) => {
-             const {isAuthenticated, toggleAuth} = authContext
-             const {nightMode, day, night} = LayoutContext
-             const mood = nightMode ?  night: day
-             return(
-               <div>
-                <div style={{background: mood.bg}}>
-                  <h2>gopay</h2>
+        {authContext => (
+          <LayoutContext.Consumer>
+            {LayoutContext => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { nightMode, day, night } = LayoutContext;
+              const mood = nightMode ? night : day;
+              return (
+                <div>
+                  <div style={{ background: mood.bg }}>
+                    <h2>gopay</h2>
+                  </div>
+                  <button onClick={toggleAuth}>
+                    {isAuthenticated ? <p>logged in </p> : <p>Logged out</p>}
+                  </button>
                 </div>
-                <button onClick={toggleAuth}>
-                  {isAuthenticated? <p>logged in </p> : <p>Logged out</p>}
-                </button>
-               </div>
-             )
-           }}
-         </LayoutContext.Consumer>
+              );
+            }}
+          </LayoutContext.Consumer>
         )}
       </AuthContext.Consumer>
     );
